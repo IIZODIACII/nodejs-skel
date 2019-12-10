0b94bb878a586382b3e47cf837a876700ea697f0
IIZODIACII <amr_x14@outlook.com>
2019-12-10 22:36:07 +0200

    initial commit

---
+++ db/db.js
@@ -0,0 +1,18 @@
+const data = [
+  {
+    id: 1,
+    name: "n1",
+    password: "123"
+  },
+  {
+    id: 2,
+    name: "n2",
+    password: "456"
+  },
+  {
+    id: 3,
+    name: "n3",
+    password: "789"
+  }
+];
+export default data;
